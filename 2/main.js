d8c123c5df830c2f1b577ec6a29662344ad4ef6f
maksim <bebraedus220@gmail.com>
2025-09-19 02:40:48 +0300

    first commit

---
+++ 2/main.js
@@ -0,0 +1,8 @@
+bash
+
+git init
+git add -A       
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:maksimjonjon/ivanov-maksim-js.git
+git push -u origin main
